736e35a615e9ef90d93cb37b40b7d4bb96bbc0b6
yongjiksim <126915042+yongjiksim@users.noreply.github.com>
2023-03-13 05:29:45 +0900

    노드

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello ssWorld!')
+})
+
+app.get('/dog', (req, res) => {
+    res.send('강아지')
+})
+app.get('/cat', (req, res) => {
+    res.send('고양이')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
